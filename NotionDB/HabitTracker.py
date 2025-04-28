54aa49592e716a92629f67bb61edbb24e64b5407
QWERTOP18 <mz18yui@gmail.com>
2025-04-28 15:09:20 +0900

    Chinese-word

--- NotionDB/HabitTracker.py
+++ NotionDB/HabitTracker.py
@@ -1,12 +1,11 @@
 import requests
 import os
 import dotenv
-dotenv.load_dotenv("../.env")
 
+dotenv.load_dotenv("../.env")
 
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 DATABASE_ID = os.getenv("DATABASE_ID")
-
 
 url = f'https://api.notion.com/v1/databases/{DATABASE_ID}/query'
 
@@ -15,14 +14,17 @@ headers = {
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json"
 }
-print(NOTION_TOKEN)
-print(DATABASE_ID)
 
 response = requests.post(url, headers=headers)
 
 if response.status_code == 200:
     data = response.json()
     for result in data["results"]:
-        print(result["properties"])
+        properties = result["properties"]
+        word = properties["單詞"]["title"][0]["plain_text"] if properties["單詞"]["title"] else ""
+        pinyin = properties["拼音"]["rich_text"][0]["plain_text"] if properties["拼音"]["rich_text"] else ""
+        meaning = properties["意思"]["rich_text"][0]["plain_text"] if properties["意思"]["rich_text"] else ""
+
+        print(f"【単語】 {word}\n【拼音】 {pinyin}\n【意味】 {meaning}\n")
 else:
     print("エラー:", response.status_code, response.text)
